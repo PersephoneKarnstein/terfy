da2c38a94a4a8668ef29ee3e98bcf90f0d379cc1
Persephone Karnstein <persephone@Persephones-MacBook-Pro.local>
2023-06-17 18:00:25 -0700

    only punctuates and transcribes alex jones if he talks about trans people

--- src/terfy/clean_infowars.py
+++ src/terfy/clean_infowars.py
@@ -7,25 +7,32 @@ model = PunctuationModel()
 def clean_text():
     path = os.getcwd()
     files = glob.glob(path + '/../infowars/*.txt')
-    data = ""
-    timestamp = re.compile(r'^\s*\[\d{1,2}(:\d{2})?:\d{2}\.\d{3} --> \d{1,2}(:\d{2})?:\d{2}\.\d{3}\]\s+')
-    with alive_bar(len(files)) as bar:
-        for f in files:
-            result = ""
-            for line in open(f).readlines():
-                result += " "+timestamp.sub('', line).strip() #remove the timestamps
-            result = model.restore_punctuation(result)
-            data += result
-            bar()
-    return data
-
-def save_to_corpus(text):
-    path = os.getcwd()
     path += "/training-texts/alexjones.txt"
     if os.path.exists(path):
         os.remove(path)
-    with open(path, "w") as f:
-        f.write(text)
+    timestamp = re.compile(r'^\s*\[\d{1,2}(:\d{2})?:\d{2}\.\d{3} --> \d{1,2}(:\d{2})?:\d{2}\.\d{3}\]\s+')
+    trans = re.compile(r'(trans ((man)|(woman)|(people)|(ideology)|(men)|(women)|(child)|(children)))|(transgender)|(transsexual)|(tranny)|(transvestite)')
+    i,j=0,0
+    try:
+        with alive_bar(len(files)+1, title="\033[38;5;14m[INFO]\033[0m Cleaning transcripts...".ljust(35)) as bar:
+            bar()
+            with open(path, "w+") as g:
+                for f in files:
+                    result = ""
+                    for line in open(f).readlines():
+                        result += " "+timestamp.sub('', line).strip() #remove the timestamps
+                    #check if the show talks about trans people; only punctuate if it does.
+                    mentioned_trans = len(re.findall(trans, result))
+                    if mentioned_trans > 5:
+                        print(f"\033[1;38;5;15m[INFO]\033[0m Mentioned trans people {mentioned_trans} times. Saving.")
+                        result = model.restore_punctuation(result) #re-insert punctuation
+                        g.write(result)
+                        j+=1
+                    i+=1
+                    bar()
+    except KeyboardInterrupt:
+        print(f"\033[38;5;225m[CANCELLED]\033[0m {i} shows were read; {j} were transcribed.".ljust(35))
+    return
+    
 
 data = clean_text()
-save_to_corpus(data)
