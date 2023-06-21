400c06a33c7b6178116ebf2ebe807bbba6e90f1b
Persephone Karnstein <persephone@Persephones-MacBook-Pro.local>
2023-06-21 12:20:38 -0700

    fixed a path error in model.save_model

--- terfy/model.py
+++ terfy/model.py
@@ -92,24 +92,24 @@ def get_corpus_data():
 		data += open(f).read()
 	return data
 
-def save_model(model):
+def save_model(model,filepath="models"):
 	# serialize model to JSON
 	model_json = model.to_json()
-	with open("models/model.json", "w") as json_file:
+	with open(path+'/'+filepath+'/model.json', "w") as json_file:
 		json_file.write(model_json)
 	# serialize weights to HDF5
-	model.save_weights("model.h5")
+	model.save_weights(path+'/'+filepath+"/model.h5")
 	# print("Saved model to disk")
 
-def load_model(filename="models"):
+def load_model(filepath="models"):
 	path = os.getcwd()
 	with redirect_stdout(open(os.devnull, 'w')):
-		json_file = open(path+'/'+filename+'/model.json', 'r')
+		json_file = open(path+'/'+filepath+'/model.json', 'r')
 		loaded_model_json = json_file.read()
 		json_file.close()
 		loaded_model = model_from_json(loaded_model_json)
 		# load weights into new model
-		loaded_model.load_weights(path+'/'+filename+"/model.h5")
+		loaded_model.load_weights(path+'/'+filepath+"/model.h5")
 	print("Loaded model from disk")
 	return loaded_model
 
